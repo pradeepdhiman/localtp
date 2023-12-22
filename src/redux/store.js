476b0f83f8edaf36e7e49af3c4ed59869cfb5abf
gooperofficial <pradeepdhiman45@gmail.com>
2023-12-22 12:38:49 +0530

    store update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { coursesApi } from "layouts/courses/functions/query";
 import emptySplitApi from "../utils/emptySplitApi";
 
 
 export const store = configureStore({
     reducer: {
-        [emptySplitApi.reducerPath]: coursesApi.reducer
+        [emptySplitApi.reducerPath]: emptySplitApi.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([emptySplitApi.middleware])
 })
